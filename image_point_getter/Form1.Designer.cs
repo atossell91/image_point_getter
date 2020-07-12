868e4964566a0f9a56019de7cafa8e8fd8d91e71
Anthony Tossell <atossell91@outlook.com>
2020-07-11 18:32:17 -0600

    Add project files.

---
+++ image_point_getter/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace image_point_getter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.l_point1 = new System.Windows.Forms.Label();
+            this.l_point2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.l_diff = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // l_point1
+            // 
+            this.l_point1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.l_point1.AutoSize = true;
+            this.l_point1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_point1.Location = new System.Drawing.Point(96, 420);
+            this.l_point1.Name = "l_point1";
+            this.l_point1.Size = new System.Drawing.Size(70, 25);
+            this.l_point1.TabIndex = 1;
+            this.l_point1.Text = "label1";
+            // 
+            // l_point2
+            // 
+            this.l_point2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.l_point2.AutoSize = true;
+            this.l_point2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_point2.Location = new System.Drawing.Point(305, 420);
+            this.l_point2.Name = "l_point2";
+            this.l_point2.Size = new System.Drawing.Size(70, 25);
+            this.l_point2.TabIndex = 2;
+            this.l_point2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 401);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 301);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // l_diff
+            // 
+            this.l_diff.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.l_diff.AutoSize = true;
+            this.l_diff.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_diff.Location = new System.Drawing.Point(508, 420);
+            this.l_diff.Name = "l_diff";
+            this.l_diff.Size = new System.Drawing.Size(70, 25);
+            this.l_diff.TabIndex = 5;
+            this.l_diff.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.l_diff);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.l_point2);
+            this.Controls.Add(this.l_point1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label l_point1;
+        private System.Windows.Forms.Label l_point2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label l_diff;
+    }
+}
+
---
+++ image_point_getter/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace image_point_getter
+{
+    public partial class Form1 : Form
+    {
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+
+        private bool shiftpressed = false;
+
+        private SolidBrush b1;
+        private SolidBrush b2;
+        public Form1()
+        {
+            InitializeComponent();
+            b1 = new SolidBrush(Color.Blue);
+            b2 = new SolidBrush(Color.Red);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey)
+            {
+                shiftpressed = true;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                if (shiftpressed)
+                {
+                    --y2;
+                    setP2Coords(x2, y2);
+                }
+                else
+                {
+                    --y1;
+                    setP1Coords(x1, y1);
+                }
+                pictureBox1.Refresh();
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                if (shiftpressed)
+                {
+                    --x2;
+                    setP2Coords(x2, y2);
+                }
+                else
+                {
+                    --x1;
+                    setP1Coords(x1, y1);
+                }
+                pictureBox1.Refresh();
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                if (shiftpressed)
+                {
+                    ++y2;
+                    setP2Coords(x2, y2);
+                }
+                else
+                {
+                    ++y1;
+                    setP1Coords(x1, y1);
+                }
+                pictureBox1.Refresh();
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                if (shiftpressed)
+                {
+                    ++x2;
+                    setP2Coords(x2, y2);
+                }
+                else
+                {
+                    ++x1;
+                    setP1Coords(x1, y1);
+                }
+                pictureBox1.Refresh();
+            }
+            calcDifference();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey)
+            {
+                shiftpressed = false;
+            }
+        }
+
+        private void calcDifference()
+        {
+            int xDiff = x2 - x1;
+            int yDiff = y2 - y1;
+
+            l_diff.Text = "Diff: (" + xDiff + ", " + yDiff + ") ";
+        }
+        private void setP1Coords(int x, int y)
+        {
+            x1 = x;
+            y1 = y;
+            l_point1.Text = "Point 1: (" + x1 + ", " + y1 + ")";
+        }
+        private void setP2Coords(int x, int y)
+        {
+            x2 = x;
+            y2 = y;
+            l_point2.Text = "Point 2: (" + x2 + ", " + y2 + ")";
+        }
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (shiftpressed)
+            {
+                setP2Coords(e.X, e.Y);
+            }
+            else
+            {
+                setP1Coords(e.X, e.Y);
+            }
+            calcDifference();
+            pictureBox1.Refresh();
+        }
+        private string getFilePath()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string output = ofd.FileName;
+            return output;
+        }
+        private void setPicture(string filepath)
+        {
+            Image img = pictureBox1.Image;
+            pictureBox1.Image = null;
+            if (img != null)
+            {
+                img.Dispose();
+            }
+            pictureBox1.Image = Image.FromFile(filepath);
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string filepath = getFilePath();
+            setPicture(filepath);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void L_point2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawCross(Graphics g, int x, int y, Brush b)
+        {
+            int top = y - 5;
+            int bottom = y + 5;
+            int left = x - 5;
+            int right = x + 5;
+
+            top = top < 0 ? 0 : top;
+            left = left < 0 ? 0 : left;
+
+            int h = pictureBox1.Height;
+            int w = pictureBox1.Width;
+
+            bottom = bottom < h ? bottom : h - 1;
+            right = right < w ? right : w - 1;
+
+            g.DrawLine(new Pen(b), x, top, x, bottom);
+            g.DrawLine(new Pen(b), left, y, right, y);
+        }
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            drawCross(e.Graphics, x1, y1, b1);
+            drawCross(e.Graphics, x2, y2, b2);
+            //e.Graphics.FillRectangle(b1, new Rectangle(x1, y1, 1, 1));
+            //e.Graphics.FillRectangle(b2, new Rectangle(x2, y2, 1, 1));
+        }
+    }
+}
