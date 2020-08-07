6f579dbabd6b11353fddebe2d34f7e973c8476ca
Anthony Tossell <atossell91@users.noreply.github.com>
2020-08-07 15:09:40 -0600

    allowed autofill

--- image_point_getter/Form1.Designer.cs
+++ image_point_getter/Form1.Designer.cs
@@ -87,11 +87,12 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Location = new System.Drawing.Point(15, 420);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load Image";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
--- image_point_getter/Form1.cs
+++ image_point_getter/Form1.cs
@@ -19,6 +19,7 @@ namespace image_point_getter
         private int y2;
 
         private bool shiftpressed = false;
+        private bool controlpressed = false;
 
         private SolidBrush b1;
         private SolidBrush b2;
@@ -33,6 +34,10 @@ namespace image_point_getter
             if (e.KeyCode == Keys.ShiftKey)
             {
                 shiftpressed = true;
+            }
+            else if (e.KeyCode == Keys.ControlKey)
+            {
+                controlpressed = true;
             }
             else if (e.KeyCode == Keys.W)
             {
@@ -99,6 +104,10 @@ namespace image_point_getter
             {
                 shiftpressed = false;
             }
+            else if (e.KeyCode == Keys.ControlKey)
+            {
+                controlpressed = false;
+            }
         }
 
         private void calcDifference()
@@ -108,23 +117,91 @@ namespace image_point_getter
 
             l_diff.Text = "Diff: (" + xDiff + ", " + yDiff + ") ";
         }
+        private void setClipBoard()
+        {
+            string s = '\t'.ToString();
+            Clipboard.SetText(x1 + s + y1 + s + x2 + s + y2);
+        }
         private void setP1Coords(int x, int y)
         {
             x1 = x;
             y1 = y;
             l_point1.Text = "Point 1: (" + x1 + ", " + y1 + ")";
+            setClipBoard();
         }
         private void setP2Coords(int x, int y)
         {
             x2 = x;
             y2 = y;
             l_point2.Text = "Point 2: (" + x2 + ", " + y2 + ")";
+            setClipBoard();
+        }
+        private int findBorder(int startX, int startY, int dir, bool isVertical, int nLimit)
+        {
+            int x;
+            int b;
+            int limit;
+
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+            if (isVertical)
+            {
+                x = startY;
+                b = startX;
+                limit = bmp.Height;
+            }
+            else
+            {
+                x = startX;
+                b = startY;
+                limit = bmp.Width;
+            }
+
+            double val = 1.0;
+            int n = 0;
+            for (; x >= 0 && x < limit && n < nLimit; x += dir)
+            {
+                if (isVertical)
+                {
+                    val = bmp.GetPixel(b, x).GetBrightness();
+                }
+                else
+                {
+                    val = bmp.GetPixel(x, b).GetBrightness();
+                }
+
+                if (val < 0.6)
+                {
+                    return x-dir;
+                }
+            }
+            return -1;
+        }
+        int findBorder(int startX, int startY, int dir, bool isVertical)
+        {
+            return findBorder(startX, startY, dir, isVertical, int.MaxValue);
+        }
+        private void setBounds(int mouseX, int mouseY)
+        {
+            int topLeftX = findBorder(mouseX, mouseY, -1, false);
+            int topLeftY = findBorder(mouseX, mouseY, -1, true);
+
+            int bottomRightX = findBorder(mouseX, mouseY, +1, false);
+            int bottomRightY = findBorder(mouseX, mouseY, +1, true);
+
+            setP1Coords(topLeftX, topLeftY);
+            setP2Coords(bottomRightX, bottomRightY);
+            this.Refresh();
         }
         private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             if (shiftpressed)
             {
                 setP2Coords(e.X, e.Y);
+            }
+            else if (controlpressed)
+            {
+                setBounds(e.X, e.Y);
             }
             else
             {
@@ -185,8 +262,14 @@ namespace image_point_getter
             g.DrawLine(new Pen(b), x, top, x, bottom);
             g.DrawLine(new Pen(b), left, y, right, y);
         }
+        private void drawRectangle(Graphics g, int x1, int y1, int width, int height)
+        {
+            Pen p = new Pen(new SolidBrush(Color.LightBlue));
+            g.DrawRectangle(p, new Rectangle(x1, y1, width, height));
+        }
         private void PictureBox1_Paint(object sender, PaintEventArgs e)
         {
+            drawRectangle(e.Graphics, x1, y1, x2 - x1, y2 - y1);
             drawCross(e.Graphics, x1, y1, b1);
             drawCross(e.Graphics, x2, y2, b2);
             //e.Graphics.FillRectangle(b1, new Rectangle(x1, y1, 1, 1));
